47f28ced6c3608342c0ac46541cbaf735b927f24
arturoyj <jayj22022email.com@gmail.com>
2025-11-01 23:13:45 -0600

    importar componentes e implementar landing-page

--- src/app/presentation/landing/landing-module.ts
+++ src/app/presentation/landing/landing-module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing-module';
-
+import { LandingPage } from './landing-page/landing-page';
+import { SharedModule } from '../../shared/shared-module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingPage
+  ],
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    SharedModule 
   ]
 })
 export class LandingModule { }
