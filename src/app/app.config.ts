97bf5193f1db910f861943f432fc9e6a8512ed40
Fullgamer494 <fullgamer494@hotmail.com>
2025-11-29 02:57:26 -0600

    Modernizar los estilos y la estructura de la Landing Page, Login y componentes compartidos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './api/interceptors/auth.interceptor';
@@ -9,15 +9,23 @@ import { EspecimenAltaPort } from './api/domain/ports/especimen-alta.port';
 import { EspecimenAltaAdapter } from './api/infrastructure/adapters/especimen-alta.adapter';
 import { ReportPort } from './api/domain/ports/report.port';
 import { ReportAdapter } from './api/infrastructure/adapters/report.adapter';
+import { EspecimenService } from './api/application/especimen.service';
+import { RegistroBajaPort } from './api/domain/ports/especimen-baja.port';
+import { EspecimenBajaAdapter } from './api/infrastructure/adapters/especimen-baja.adapter';
+import { StatisticsPort } from './api/domain/ports/statistics.port';
+import { StatisticsAdapter } from './api/infrastructure/adapters/statistics.adapter';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' })),
     provideHttpClient(withInterceptors([authInterceptor])),
 
     { provide: AuthPort, useClass: AuthAdapter },
     { provide: EspecimenAltaPort, useClass: EspecimenAltaAdapter },
-    { provide: ReportPort, useClass: ReportAdapter } 
+    { provide: ReportPort, useClass: ReportAdapter },
+    { provide: RegistroBajaPort, useClass: EspecimenBajaAdapter },
+    { provide: StatisticsPort, useClass: StatisticsAdapter },
+    { provide: EspecimenService, useClass: EspecimenService },
   ]
 };
--- src/app/presentation/landing/landing-page/landing-page.ts
+++ src/app/presentation/landing/landing-page/landing-page.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header';
 import { FooterComponent } from '../footer/footer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [ 
+  imports: [
     CommonModule,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RouterLink
   ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css'
