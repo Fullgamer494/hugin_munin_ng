8f63cb2215cde1dc9ed76f47e338fd67f4cbcb14
Fullgamer494 <fullgamer494@hotmail.com>
2025-12-08 03:56:07 -0600

    Añadir credenciales

--- database/04_HM_USERS_DATA_SEED_PGADMIN.sql
+++ database/04_HM_USERS_DATA_SEED_PGADMIN.sql
@@ -4,4 +4,4 @@ VALUES
 (2, 'Ilya', 'ilya@gmail.com', 'qwerty123', TRUE),       -- 2: Biólogo
 (3, 'Maria', 'regina@gmail.com', 'qwerty123', TRUE),      -- 3: Veterinario
 (4, 'Brittany', 'aurora@gmail.com', 'qwerty123', TRUE),   -- 4: Patólogo
-(5, 'Paul', 'paul@gmail.com', 'qwerty123', TRUE);         -- 5: Cuidador
+(5, 'Paul', 'paul@gmail.com', 'qwerty123@', TRUE);         -- 5: Cuidador
